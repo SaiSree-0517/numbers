a7060ad2ea40a408629572252acae436d0e73718
SaiSree-0517 <69571289+SaiSree-0517@users.noreply.github.com>
2021-05-12 09:19:10 +0530

    Add files via upload

---
+++ prime.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+#include<bits/stdc++.h>
+using namespace std;
+int main() 
+{
+    int i, n;
+    bool prime = true;
+    cout << "Enter a positive integer: ";
+    cin >> n;
+    if (n == 0 || n == 1) 
+	{
+        prime = false;
+    }
+    else 
+	{
+        for (i = 2; i <= n / 2; ++i) 
+		{
+            if (n % i == 0) 
+			{
+                prime = false;
+                break;
+            }
+        }
+    }
+    if (prime)
+        cout << n << " is a prime number";
+    else
+        cout << n << " is not a prime number";
+
+    return 0;
+}
+
+
+   
+
---
+++ prime1.cpp
@@ -0,0 +1,20 @@
+#include<bits/stdc++.h> 
+using namespace std;  
+int main()  
+{  
+  int n,i,count=1;  
+  cout << "Enter the Number to check Prime: ";  
+  cin >> n;   
+  for(i=2;i<=n/2;i++)  
+  {  
+      if(n%i==0)  
+      {  
+          cout<<"Number is not Prime."<<endl;  
+          count=0;  
+          break;  
+      }  
+  }  
+  if (count)  
+      cout << "Number is Prime."<<endl;  
+  return 0;  
+}  
